043964ece9e5519180272ea4132c6c08ef15a882
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2018-03-10 14:30:46 +0100

    Add all necessary functions right before the challenge

--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,42 +1,83 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
-import app from './App';
+import {
+  showFormMsg,
+  mealInputMsg,
+  caloriesInputMsg,
+  saveMealMsg,
+} from './Update';
 
-const { pre, div, h1, button, label, input, form } = hh(h);
+const { pre, div, h1, button, form, label, input } = hh(h);
 
-function fieldSet(labelText, inputValue) {
-  return div(
-    [
-      label({ className: 'db mb1' }, labelText),
-      input({
-        className: 'pa2 input-reset ba w-100 mb2',
-        type: 'text',
-        value: inputValue,
-      })
-    ],
-  );
+function fieldSet(labelText, inputValue, oninput) {
+  return div([
+    label({ className: 'db mb1' }, labelText),
+    input({
+      className: 'pa2 input-reset ba w-100 mb2',
+      type: 'text',
+      value: inputValue,
+      oninput
+    }),
+  ]);
+}
+
+function buttonSet(dispatch) {
+  return div([
+    button(
+      {
+        className: 'f3 pv2 ph3 bg-blue white bn mr2 dim',
+        type: 'submit',
+      },
+      'Save',
+    ),
+    button(
+      {
+        className: 'f3 pv2 ph3 bn bg-light-gray dim',
+        type: 'button',
+        onclick: () => dispatch(showFormMsg(false)),
+      },
+      'Cancel',
+    ),
+  ]);
 }
 
 function formView(dispatch, model) {
-  const { description } = model;
-  return form(
+  const { description, calories, showForm } = model;
+  if (showForm) {
+    return form(
+      {
+        className: 'w-100 mv2',
+        onsubmit: e => {
+          e.preventDefault();
+          dispatch(saveMealMsg);
+        },
+      },
+      [
+        fieldSet('Meal', description,
+          e => dispatch(mealInputMsg(e.target.value))
+        ),
+        fieldSet('Calories', calories || '',
+          e => dispatch(caloriesInputMsg(e.target.value))
+        ),
+        buttonSet(dispatch),
+      ],
+    );
+  }
+  return button(
     {
-      className: 'w-100 mv2',
+      className: 'f3 pv2 ph3 bg-blue white bn',
+      onclick: () => dispatch(showFormMsg(true)),
     },
-    [
-      fieldSet('Meal', description),
-    ],
+    'Add Meal',
   );
-  // return button({ className: 'f3 pv2 ph3 bg-blue white bn' },
-  //   'Add Meal');
-};
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
-    h1({ className: 'f2 pv2 bb' }, 'Calorie counter'),
+    h1({ className: 'f2 pv2 bb' }, 'Calorie Counter'),
     formView(dispatch, model),
-    pre(JSON.stringify(model, null, 2))
+    pre(JSON.stringify(model, null, 2)),
   ]);
-};
+}
 
 export default view;
